4cf8177778f2ae5ebeeb8f13e99ee8afd440b8b3
hekekimi <148078148+hekekimi@users.noreply.github.com>
2024-06-07 15:52:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-
-
-//valitaan kaikki elemnentit
 const audiot = document.querySelectorAll("audio");
 const buttons = document.querySelectorAll(".button");
 
-//lisää tapahtumakuuntelijat
 buttons.forEach(button => {
     button.addEventListener("click",()=>{
         const index = button.getAttribute("data-index");
@@ -13,21 +9,14 @@ buttons.forEach(button => {
     
 });
 
-
 function playSound(index) {
-    // Hae äänielementti indeksin perusteella
     const audio = audiot[index];
     const button = buttons[index];
     
-    if (audio.paused) {
-        audio.load();
-        audio.play();
-        button.classList.add("onplay");
-    } else {
-        audio.pause();
-        button.classList.remove("onplay");
-    }
-
+    audio.load();
+    audio.play();
+    button.classList.add("onplay");
+    
     setTimeout(() => {
         button.classList.remove("onplay");
     }, 1500);
